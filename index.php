cbf7f1e1e13a20110655d211562dd90e8f636a51
gaetano-94 <gaetano.panico1994@gmail.com>
2024-01-15 17:33:10 +0100

    feat: instanziamento oggetti movie

--- index.php
+++ index.php
@@ -19,3 +19,6 @@ class Movie
     return "Title: {$this->title}, Genre: {$this->genre}, Release Year: {$this->releaseYear}";
   }
 }
+// Istanziamento di due oggetti 'Movie'
+$movie1 = new Movie("Inception", "Sci-Fi", 2010);
+$movie2 = new Movie("The Shawshank Redemption", "Drama", 1994);
